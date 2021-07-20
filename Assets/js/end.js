cfb451e011e33a28377506203ad7a32149d1f6fe
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-07-19 23:17:16 -0700

    added comments to each file page

--- Assets/js/end.js
+++ Assets/js/end.js
@@ -3,16 +3,18 @@ const saveScoreBtn = document.querySelector('#saveScoreBtn')
 const finalScore = document.querySelector('#finalScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 
+// use local storage to save, highScores, get mostRecentScore
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
+// stores 5 heighest scores
 const MAX_HIGH_SCORES = 5
 
 finalScore.innerText = mostRecentScore
-
+// Once something is keyed into the username field the save button will work
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value
 })
-
+// save highscore and name, push out lower score if 5 scores are already listed
 saveHighScore = e => {
     e.preventDefault()
 
--- Assets/js/highscores.js
+++ Assets/js/highscores.js
@@ -1,6 +1,6 @@
 const highScoresList = document.querySelector('#highScoresList')
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
-
+// create a list with the highscore name and score via local storage
 highScoresList.innerHTML = 
 highScores.map(score => {
     return `<li class="high-score">${score.name} - ${score.score}</li>`
