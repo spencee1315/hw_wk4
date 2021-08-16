0419d62b7c5b33039c214c6d6929eda5f99803cd
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-08-15 21:51:52 -0700

    Update game.js

--- Assets/js/game.js
+++ Assets/js/game.js
@@ -91,7 +91,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('hw_wk4/end.html')
+        return window.location.assign('./end.html')
     }
 
     questionCounter++
